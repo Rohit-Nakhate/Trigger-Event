f5242516eff8cf7caea9dd0cbf9406f34721f747
Rohit-Nakhate <130633843+Rohit-Nakhate@users.noreply.github.com>
2023-06-12 18:33:39 +0530

    Add files via upload

---
+++ names_of_repositories.py
@@ -0,0 +1,17 @@
+from github import Github
+# Replace with your GitHub App private key path
+PRIVATE_KEY_PATH = 'x48vxihNr4IJQDYKpCCFnLTIMUQwj8Vab9gkl1cOPmk='
+# Replace with your GitHub App ID
+APP_ID = '346211'
+
+def main():
+    # Create a GitHub instance using your GitHub App private key and app ID
+    g = Github(APP_ID, private_key=PRIVATE_KEY_PATH)
+
+    # Implement your GitHub App functionality
+    # Example: Print the names of repositories accessible to the app
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+
+if __name__ == "__main__":
+    main()
